5288d142df4625d6eb52e5c7168a2b0824e3f915
ErnoMitrovic <a01656828@itesm.mx>
2020-09-08 08:35:18 -0500

    Added all five methods
    init()
    init_loop()
    loop()
    start()
    stop()

--- Teleoperado.java
+++ Teleoperado.java
@@ -1,1 +1,30 @@
 /* Insert code here */
+package org.firstinspires.ftc.teamcode.capacitaciones;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp(name="Teleoperado")
+
+public class Teleoperado extends OpMode{
+    @Override
+    public void init(){
+        telemetry.addData("Hola", " Mundo");
+    }
+    @Override
+    public void init_loop(){
+
+    }
+    @Override
+    public void loop(){
+
+    }
+    @Override
+    public void start(){
+
+    }
+    @Override
+    public void stop(){
+
+    }
+}
