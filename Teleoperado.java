cc5b6461cae33abeb1abf5ec4162c7f0d7eb6782
ErnoMitrovic <a01656828@itesm.mx>
2020-09-14 20:14:12 -0500

    Sesión 14 sept 2020
    Actualizar la clase para implementar motores.

--- Teleoperado.java
+++ Teleoperado.java
@@ -1,15 +1,24 @@
-/* Insert code here */
 package org.firstinspires.ftc.teamcode.capacitaciones;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @TeleOp(name="Teleoperado")
 
 public class Teleoperado extends OpMode{
+    /*
+    * En la siguiente sesión, debemos implementar el metodo clip de la librería
+    * com.qualcomm.robotcore.util
+    */
+    public DcMotor motor;
     @Override
     public void init(){
         telemetry.addData("Hola", " Mundo");
+        hardwareMap.get(DcMotor.class, "Motor_derecho");
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setDirection(DcMotor.Direction.FORWARD);
     }
     @Override
     public void init_loop(){
@@ -17,7 +26,7 @@ public class Teleoperado extends OpMode{
     }
     @Override
     public void loop(){
-
+        motor.setPower(gamepad1.left_stick_x);
     }
     @Override
     public void start(){
