41f51e2b593af9787cf8763d0b56c62d62bfd885
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2020-09-22 18:45:23 -0500

    Prueba de teleoperado

--- Teleoperado.java
+++ Teleoperado.java
@@ -1,25 +1,24 @@
 package org.firstinspires.ftc.teamcode.capacitaciones;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Teleoperado")
 
 public class Teleoperado extends OpMode{
-    /*
-    * En la siguiente sesión, debemos implementar el metodo clip de la librería
-    * com.qualcomm.robotcore.util
-    */
-    public DcMotor motor;
+    public DcMotor upRight;
+    public DcMotor upLeft;
+    public DcMotor downRight;
+    public DcMotor downLeft;
     @Override
     public void init(){
-        telemetry.addData("Hola", " Mundo");
-        hardwareMap.get(DcMotor.class, "Motor_derecho");
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor.setDirection(DcMotor.Direction.FORWARD);
+        upRight = hardwareMap.get(DcMotor.class, "arriba_derecho");
+        downRight = hardwareMap.get(DcMotor.class, "abajo_derecho");
+        upLeft = hardwareMap.get(DcMotor.class, "arriba_izquierdo");
+        downLeft = hardwareMap.get(DcMotor.class, "abajo_izquierdo");
     }
     @Override
     public void init_loop(){
@@ -27,16 +26,22 @@ public class Teleoperado extends OpMode{
     }
     @Override
     public void loop(){
-        motor.setPower(gamepad1.left_stick_x);
-        int limit = Range.clip(motor.getCurrentPosition(),0,2450);
-        telemetry.addData("Clip output:", limit);
+        double drive, turn, lateral;
+        drive = -gamepad1.left_stick_y;
+        turn = gamepad1.left_stick_x;
+        lateral = gamepad1.right_stick_x;
+        //Con el método clip solo se admiten los valores entre -1 y 1
+        double upRightPower = Range.clip(drive - turn - lateral, -1, 1);
+        double upLeftPower = Range.clip(drive + turn + lateral, -1, 1);
+        double downRightPower = Range.clip(drive - turn + lateral, -1, 1);
+        double downLeftPower = Range.clip(drive + turn - lateral, -1, 1);
     }
     @Override
     public void start(){
-
+        
     }
     @Override
     public void stop(){
-
+        
     }
 }
